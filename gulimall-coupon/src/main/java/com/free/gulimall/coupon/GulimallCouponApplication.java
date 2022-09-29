9703a87095f48778d61565ab03adbb2075c0a669
free <hunter.com>
2022-09-29 21:37:58 +0800

    项目初始结构

---
+++ gulimall-coupon/src/main/java/com/free/gulimall/coupon/GulimallCouponApplication.java
@@ -0,0 +1,13 @@
+package com.free.gulimall.coupon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GulimallCouponApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GulimallCouponApplication.class, args);
+    }
+
+}
